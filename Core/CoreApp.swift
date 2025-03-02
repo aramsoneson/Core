97f204f745f46fd640ceddcf05be36394265fa74
Aram Soneson <aram.soneson@icloud.com>
2025-03-01 20:10:50 -0500

    Initial Commit

---
+++ Core/CoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreApp.swift
+//  Core
+//
+//  Created by Aram Soneson on 3/1/25.
+//
+
+import SwiftUI
+
+@main
+struct CoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoreTests/CoreTests.swift
@@ -0,0 +1,17 @@
+//
+//  CoreTests.swift
+//  CoreTests
+//
+//  Created by Aram Soneson on 3/1/25.
+//
+
+import Testing
+@testable import Core
+
+struct CoreTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
